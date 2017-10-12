716632399754980fe48365cd51af4295b4aa69ef
Julio <julio.grillo98@gmail.com>
2017-10-12 14:24:52 -0700

    caddy to stdout

--- ear7h-net.go
+++ ear7h-net.go
@@ -3,11 +3,14 @@ package main
 import (
 	"github.com/ear7h/ear7h-net/api"
 	"os/exec"
+	"os"
 )
 
 func main() {
 	go api.Main()
-	exec.Command("caddy").Start()
+	cmd := exec.Command("caddy")
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
 
 	hang := make(chan bool)
 
